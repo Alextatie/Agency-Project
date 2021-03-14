52652cdb8988c47c6f8fe4f7273cbe1cf0930e43
vladpr31 <vladip31@gmail.com>
2021-03-14 14:29:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app_port = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendfile("./src/index.html");
+    res.sendfile("./src/Index.HTML");
 });
 
 app.listen(app_port)
