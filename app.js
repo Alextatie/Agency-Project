0a37cebc3005da161fd0e9d7a50ae9874bd7c974
Alex Tatievsky <alextatie@gmail.com>
2021-03-20 13:37:53 +0200

    valid

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //iomports
 const express  = require('express')
+const bodyParser = require('body-parser');
+const {check,validationResult}=require('express-validator')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -13,7 +15,9 @@ app.use('/bg-1',express.static(__dirname+'public/img'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-//get
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+
+//navigation
 app.get('',(req,res)=>{
   res.render('index')
 })
@@ -36,6 +40,24 @@ app.get('/login',(req,res)=>{
 
 app.get('/signup',(req,res)=>{
   res.render('signup')
+})
+
+app.post('/signup',urlencodedParser,[
+  check('username','Username must be 3+ characters long')
+    .exists()
+    .isLength({min: 3}),
+  check('email','Email is not valid')
+    .isEmail()
+    .normalizeEmail()
+],(req,res)=>{
+  const errors = validationResult(req)
+
+  if(!errors.isEmpty()){
+    const alert = errors.array()
+    res.render('signup', {
+      alert
+    })
+  }
 })
 
 
