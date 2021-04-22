087f6482ac128b2fa22538ff7dcf4b47782ba424
Alextatie <57321474+Alextatie@users.noreply.github.com>
2021-04-23 00:53:45 +0300

    newdb

--- .circleci/config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-const { secretOrKey } = require("./keys_prod");
-
-module.exports={
- mongoURI:'mongodb+srv://agency_admin:<Avi123963>@cluster0.jl6xd.mongodb.net/Travel_Agency?retryWrites=true&w=majority',
- secretOrKey:'secret'
-};
